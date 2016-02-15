c67bdfc40061ea45cf73a3c5b60941c1278e4f07
Alexander Usov <a.usoff@gmail.com>
2016-02-15 19:03:38 +0200

    First commit

---
+++ src/main/java/com/codenjoy/dojo/snake/client/YourSolver.java
@@ -0,0 +1,109 @@
+package com.codenjoy.dojo.snake.client;
+
+
+import com.codenjoy.dojo.client.Direction;
+import com.codenjoy.dojo.client.Solver;
+import com.codenjoy.dojo.client.WebSocketRunner;
+import com.codenjoy.dojo.services.Dice;
+import com.codenjoy.dojo.services.Point;
+import com.codenjoy.dojo.services.RandomDice;
+import com.codenjoy.dojo.snake.model.Elements;
+
+/**
+ * User: your name
+ */
+public class YourSolver implements Solver<Board> {
+
+    private static final String USER_NAME = "a.usoff@gmail.com";
+
+    private Dice dice;
+    private Board board;
+
+    public YourSolver(Dice dice) {
+        this.dice = dice;
+    }
+
+    @Override
+    public String get(Board board) {
+        this.board = board;
+
+//        Point point = board.getApples().get(0);
+//        point.getX()
+//        point.getY()
+
+        char[][] field = board.getField();
+
+        // found snake
+        int snakeHeadX = -1;
+        int snakeHeadY = -1;
+        for (int x = 0; x < field.length; x++) {
+            for (int y = 0; y < field.length; y++) {
+                char ch = field[x][y];
+                if (ch == Elements.HEAD_DOWN.ch() ||
+                    ch == Elements.HEAD_UP.ch() ||
+                    ch == Elements.HEAD_LEFT.ch() ||
+                    ch == Elements.HEAD_RIGHT.ch())
+                {
+                    snakeHeadX = x;
+                    snakeHeadY = y;
+                    break;
+
+                }
+            }
+            if (snakeHeadX != -1) {
+                break;
+            }
+        }
+
+        // нашли змейку
+        int appleX = -1;
+        int appleY = -1;
+        for (int x = 0; x < field.length; x++) {
+            for (int y = 0; y < field.length; y++) {
+                char ch = field[x][y];
+                if (ch == Elements.GOOD_APPLE.ch()) {
+                    appleX = x;
+                    appleY = y;
+                    break;
+
+                }
+            }
+            if (appleX != -1) {
+                break;
+            }
+        }
+
+        int dx = snakeHeadX - appleX;
+        int dy = snakeHeadY - appleY;
+
+        if (dx < 0) {
+            return Direction.RIGHT.toString();
+        }
+        if (dx > 0) {
+            return Direction.LEFT.toString();
+        }
+        if (dy < 0) {
+            return Direction.DOWN.toString();
+        }
+        if (dy > 0) {
+            return Direction.UP.toString();
+        }
+
+        return Direction.UP.toString();
+    }
+
+    public static void main(String[] args) {
+        start(USER_NAME, WebSocketRunner.Host.REMOTE);
+    }
+
+    public static void start(String name, WebSocketRunner.Host server) {
+        try {
+            WebSocketRunner.run(server, name,
+                    new YourSolver(new RandomDice()),
+                    new Board());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
